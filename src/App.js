747fadc18feb3aa6eccb4175f502541b464fa691
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-18 23:50:35 -0300

    comentario-title

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App() {
       </Modal>
      <GalleryModal 
       isOpen={isOpenGalleryModal}
-      title="Gallery"
       isClose={closeGalleryModal}
      ></GalleryModal>
     </div>
--- src/components/GalleryModal.js
+++ src/components/GalleryModal.js
@@ -1,9 +1,16 @@
 import Modal from "./Modal";
 
 export default function GalleryModal(props) {
+
+  /* 
+  <Modal title="New Gallery" {...props} > 
+    de venir un title desde App.js New Gallery seria reemplazado por ese title
+  <Modal {...props} title="New Gallery"></Modal> 
+    de venir un title desde App.js New Gallery lo sobre escribiria
+  */
   return (
-        <Modal {...props}>
-            <h1>Photo</h1>
-        </Modal>
-    )
+    <Modal {...props} title="New Gallery">
+      <h1>Photo</h1>
+    </Modal>
+  );
 }
